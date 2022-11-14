4fee0e4ce73958ef33948bdee9d0dc1b634d4d43
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-11-14 12:24:09 -0700

    posts are successful

--- controllers/api/blog-routes.js
+++ controllers/api/blog-routes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const { Blogs } = require('../../models');
 
-
-
+router.post('/create', async (req, res) => {
+    try {
+      const dbPostData = await Blogs.create({
+        title: req.body.title,
+        description: req.body.description,
+        user_id: req.body.user_id,
+      });
+      res.status(200).json(dbPostData)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    });
 
 module.exports = router;
